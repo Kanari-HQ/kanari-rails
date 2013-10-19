aed214a3028cc5c2ed196da9543b9e4892857f20
kamilski81 <kamilski81@gmail.com>
2013-10-19 13:13:17 -0500

    add voting mechanism

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131019161017) do
+ActiveRecord::Schema.define(version: 20131019164843) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,5 +25,14 @@ ActiveRecord::Schema.define(version: 20131019161017) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  create_table "votes", force: true do |t|
+    t.string   "vote_type"
+    t.integer  "event_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "votes", ["event_id"], name: "index_votes_on_event_id", using: :btree
 
 end
