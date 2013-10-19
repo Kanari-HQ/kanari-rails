ca6fcef6441de6531a903a8811332ccd5db0a1d0
kamilski81 <kamilski81@gmail.com>
2013-10-18 22:04:38 -0500

    add secret token

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Audienceanalog::Application.config.secret_key_base = '6f7b1af475e36ee5f60abcf2b10f41c4ff24e9c16be470890eb97c882ea59cdc2a9aff624ffc9d4cf924b874cb76ec50335d6a7ed956eedf6c38f3c06974f394'
