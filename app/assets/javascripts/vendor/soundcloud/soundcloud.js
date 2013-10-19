b6fcfa625178788d214534fd10fe0bfb6beed046
mrilikecoding <nathanrgreen@gmail.com>
2013-10-19 14:33:54 -0500

    update sc js config

--- app/assets/javascripts/vendor/soundcloud/soundcloud.js
+++ app/assets/javascripts/vendor/soundcloud/soundcloud.js
@@ -1,4 +1,11 @@
+//SC.initialize({
+//  client_id: "f7cb0fd36d4e70322ccdff9f2d8729b1",
+//  redirect_uri: "http://localhost:3000/callback.html"
+//});
+
+
 SC.initialize({
-  client_id: "f7cb0fd36d4e70322ccdff9f2d8729b1",
-  redirect_uri: "http://localhost:3000/callback.html"
+    client_id: "f7cb0fd36d4e70322ccdff9f2d8729b1",
+    redirect_uri: "http://messieurslikerumbles.r13.railsrumble.com/callback.html"
 });
+
