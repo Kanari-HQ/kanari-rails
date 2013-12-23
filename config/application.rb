2c06f42799259260956b8d529747b364bd60da1d
mrilikecoding <nathanrgreen@gmail.com>
2013-12-23 11:47:48 -0500

    config for heroku

--- config/application.rb
+++ config/application.rb
@@ -24,6 +24,9 @@ module Kanari
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
 
+
+    config.assets.initialize_on_precompile = false
+
     config.generators do |g|
       g.fixture_replacement :factory_girl
     end
